99011e03f943c6aa72a54987d4bc67295dbde343
Wagner Ferreira <ferreirasi.tk@outlook.com>
2024-02-07 15:01:55 -0300

    WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 
+import jQuery from 'jquery';
+window.$ = jQuery;
+
 import { createApp } from 'vue';
-import Select2 from 'vue3-select2-component';
 
 const app = createApp({});
 
@@ -9,5 +11,4 @@ Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, 
     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
 });
 
-app.component('Select2', Select2)
 app.mount('#app')
---
+++ resources/js/components/Exemple.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container">
+        <h1 class="text-center text-3xl text-yellow-500">
+            Exemple Component
+        </h1>
+    </div>
+</template>
