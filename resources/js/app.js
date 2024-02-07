b29b27d2b371ea539a1c1f6cc0e3c27207cfcbbb
Wagner Ferreira <ferreirasi.tk@outlook.com>
2024-02-07 14:35:03 -0300

    WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,13 @@
 import './bootstrap';
 
 import { createApp } from 'vue';
+import Select2 from 'vue3-select2-component';
 
-// import components from './components' dinamically
+const app = createApp({});
 
-
-const app = createApp({
-
+Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
+    app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
 });
 
+app.component('Select2', Select2)
 app.mount('#app')
